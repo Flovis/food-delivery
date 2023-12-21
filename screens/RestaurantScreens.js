27a7e54341e59f04cb6c55634923c3f6317b62b4
flovis <floviskabndafkm@gmail.com>
2023-12-21 20:01:25 +0200

    first commit

---
+++ screens/RestaurantScreens.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RestaurantScreens() {
+  return (
+    <View>
+      <Text>RestaurantScreens</Text>
+    </View>
+  )
+}
